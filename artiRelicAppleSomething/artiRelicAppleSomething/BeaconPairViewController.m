4ac8eade525551cc380fa14c0e599bd07b10227d
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-08-09 11:15:49 -0700

    add audio to newPieceController

--- artiRelicAppleSomething/artiRelicAppleSomething/BeaconPairViewController.m
+++ artiRelicAppleSomething/artiRelicAppleSomething/BeaconPairViewController.m
@@ -8,9 +8,17 @@
 
 #import "BeaconPairViewController.h"
 #import "Beacon.h"
+#import <EstimoteSDK/EstimoteSDK.h>
 
-@interface BeaconPairViewController () <UITableViewDataSource>
+// Estomote
+#import "BeaconDetails.h"
+#import "BeaconDetailsCloudFactory.h"
+#import "CachingContentFactory.h"
+#import "ProximityContentManager.h"
 
+@interface BeaconPairViewController () <UITableViewDataSource, ProximityContentManagerDelegate>
+
+@property (nonatomic) ProximityContentManager *proximityContentManager;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) NSArray *availableBeacons;
 
@@ -24,13 +32,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.tableView.dataSource = self;
-
+    
+    // this seems expensive to put this here...but it is only called once here?
+    self.proximityContentManager = [[ProximityContentManager alloc]
+                                    initWithBeaconIDs:@[
+                                                        [[BeaconID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D" major:7340 minor:18322],
+                                                        [[BeaconID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D" major:43557 minor:26950],
+                                                        [[BeaconID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D" major:32193 minor:28874]
+                                                        ]
+                                    beaconContentFactory:[[CachingContentFactory alloc] initWithBeaconContentFactory:[BeaconDetailsCloudFactory new]]];
+    self.proximityContentManager.delegate = self;
+    
+    [self.proximityContentManager startContentUpdates];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     // setup beacon call to see what beacons are available here.
-    //
+    // [self fetchBeaconData];
+}
+
+- (void)proximityContentManager:(ProximityContentManager *)proximityContentManager didUpdateContent:(id)content {
+//    [self.activityIndicator stopAnimating];
+//    [self.activityIndicator removeFromSuperview];
+    
+    BeaconDetails *beaconDetails = content;
+    if (beaconDetails) {
+//        self.view.backgroundColor = beaconDetails.backgroundColor;
+//        self.label.text = [NSString stringWithFormat:@"You're in %@'s range!", beaconDetails.beaconName];
+//        self.image.hidden = NO;
+    } else {
+//        self.view.backgroundColor = BeaconDetails.neutralColor;
+//        self.label.text = @"No beacons in range.";
+//        self.image.hidden = YES;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,6 +75,9 @@
 - (IBAction)saveButtonPressed:(id)sender {
     
 }
+
+#pragma MARK - Estimote Location Management
+
 
 #pragma MARK - UITableViewDataSource
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
