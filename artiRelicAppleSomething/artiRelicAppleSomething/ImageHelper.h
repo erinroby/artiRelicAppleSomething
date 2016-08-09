b1839e3f737a7be3cc9d988dfbe6fb8c275607fc
Jeremy Moore <damoores@gmail.com>
2016-08-09 10:59:33 -0700

    add imageHelper class

---
+++ artiRelicAppleSomething/artiRelicAppleSomething/ImageHelper.h
@@ -0,0 +1,15 @@
+//
+//  ImageHelper.h
+//  artiRelicAppleSomething
+//
+//  Created by Jeremy Moore on 8/9/16.
+//  Copyright © 2016 Erin Roby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <UIKit/UIKit.h>
+
+@interface ImageHelper : NSObject
+
+@end
---
+++ artiRelicAppleSomething/artiRelicAppleSomething/ImageHelper.m
@@ -0,0 +1,35 @@
+//
+//  ImageHelper.m
+//  artiRelicAppleSomething
+//
+//  Created by Jeremy Moore on 8/9/16.
+//  Copyright © 2016 Erin Roby. All rights reserved.
+//
+
+#import "ImageHelper.h"
+
+@implementation ImageHelper
+
+
+-(UIImage *)thumbFromImage:(UIImage *)image
+{
+
+    CGSize thumbSize = CGSizeMake(50.0, 50.0);
+    UIGraphicsBeginImageContext(thumbSize);
+    [image drawInRect:CGRectMake(0.0, 0.0, thumbSize.width, thumbSize.height)];
+    UIImage *thumbImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return thumbImage;
+}
+
+-(NSData*)dataFromImage:(UIImage *)image
+{
+    return UIImageJPEGRepresentation(image, 0.7);
+}
+
+-(UIImage *)imageFromdata:(NSData *)data
+{
+    return [UIImage imageWithData:data];
+}
+
+@end
